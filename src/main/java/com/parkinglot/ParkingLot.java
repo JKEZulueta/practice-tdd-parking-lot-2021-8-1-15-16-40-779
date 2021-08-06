25b631055c7c23fccaa3c1d527003f14fb97f1c3
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-06 14:55:36 +0800

    [ParkingLot][ZULUEJA] feat: implemented should_return_right_car_with_ticket_when_fetch_car_twice_given_a_parking_lot_with_two_parked_cars_and_two_parking_tickets

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,16 +1,22 @@
 package com.parkinglot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
-    private Car car;
+    private final Map<ParkingTicket, Car> parkedPosition = new HashMap<>();
+
 
 
     public ParkingTicket park(Car car) {
-        this.car = car;
-        return new ParkingTicket();
+        ParkingTicket parkingTicket = new ParkingTicket();
+        parkedPosition.put(parkingTicket, car);
+
+        return parkingTicket;
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
         ////Given a parking lot with a parked car, and a parking ticket, when fetch the car twice, then return the right car with each ticket
-        return car;
+        return parkedPosition.get(parkingTicket);
     }
 }
--- src/main/java/com/parkinglot/ParkingTicket.java
+++ src/main/java/com/parkinglot/ParkingTicket.java
@@ -2,4 +2,6 @@ package com.parkinglot;
 
 public class ParkingTicket {
 
+
+
 }
