0a7fe4f332d70e8f2584938cd3690bdef8174e7b
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-06 14:16:34 +0800

    [ParkingLot][ZULUEJA] test: added should_return_parking_ticket_when_park_given_a_parking_lot_and_a_car

---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,20 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingLotTest {
+    @Test
+    void should_return_parking_ticket_when_park_given_a_parking_lot_and_a_car() {
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+
+        //When
+        ParkingTicket parkingTicket = parkingLot.park(car);
+
+        //Then
+        assertNotNull(parkingTicket);
+    }
+}
