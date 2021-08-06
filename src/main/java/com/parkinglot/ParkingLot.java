81eb0d89eea728acc3c0adb31a27d9b03ff6be97
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-06 14:30:55 +0800

    [ParkingLot][ZULUEJA] feat: implementation should_return_parking_ticket_when_park_given_a_parking_lot_and_a_parked_car_and_a_parking_ticket

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,15 +1,16 @@
 package com.parkinglot;
 
 public class ParkingLot {
+    private Car car;
 
 
     public ParkingTicket park(Car car) {
-//        return null;
+        this.car = car;
         return new ParkingTicket();
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
         ////Given a parking lot with a parked car, and a parking ticket, when fetch the car twice, then return the right car with each ticket
-        return null;
+        return car;
     }
 }
