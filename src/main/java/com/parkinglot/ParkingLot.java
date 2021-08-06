c3d8f2e9f37ce99f07aa39b1894b5315243ea4ce
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-06 14:27:16 +0800

    [ParkingLot][ZULUEJA] refact: transferred java files into java folder instead of the test folder

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -5,12 +5,11 @@ public class ParkingLot {
 
     public ParkingTicket park(Car car) {
 //        return null;
-
-
         return new ParkingTicket();
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
+        ////Given a parking lot with a parked car, and a parking ticket, when fetch the car twice, then return the right car with each ticket
         return null;
     }
 }
